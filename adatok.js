c0b5a355f0b9e7e9da2057d8b5fac5e0cc9006ee
DDorka02 <dobszaydorka@gmail.com>
2024-08-29 14:46:51 +0200

    javítás adatok és css-be

--- adatok.js
+++ adatok.js
@@ -2,194 +2,164 @@ export const SZOBAK = [
     {
         id: F1/1,
         max:2,
-        elso:Eszter,
-        masodik: Buszti,
-        harmadik: Sajka
-        
+        lakok: ['Eszter','Buszti','Sajka']        
     },
 
     {
         id: F1/2,
         max:2,
-        elso:Dávid
+        lakok:['Dávid']
         
     },
 
     {
         id: F2/1,
         max:2,
-        elso:Luca,
-        masodik: Adorján
+        lakok:['Luca','Adorján']
+       
         
     },
     {
         id: F2/2,
         max:2,
-        elso:Veronika,
-        masodik: Rozi,
-        harmadik: Adél
+        lakok:['Veronika','Rozi','Adél']
+        
         
     },
     {
         id: F3/1,
         max:2,
-        elso:Marcsi,
-        masodik: F. Matyi,
-        harmadik: Kinga 
+        lakok:['Marcsi','F. Matyi','Kinga']
         
     },
     {
         id: F3/2,
         max:2,
-        elso:Bálint,
-        masodik: Noémi
-        
+        lakok:['Bálint','Noémi']
     },
     {
         id: F3/1,
         max:1,
-        elso:Matyi
+        lakok:['Matyi']
         
     },
     {
         id: F4/1,
         max:2,
-        elso:Miklós,
-        masodik: Timi,
-        harmadik: Beni
+        lakok:['Miklós','Timi','Beni']
+        
         
     },
     {
         id: F4/2,
         max:2,
-        elso:Lívia,
-        masodik: Vili
+        lakok:['Lívia','Vili']
+        
         
     },
     {
         id: Fszt1,
         max:3,
-        elso:Zsófi,
-        masodik: András,
-        harmadik: Marci,
-        negyedik: Misi,
-        otodik: Juli
+        lakok:['Zsófi','András','Marci','Misi','Juli']
+        
         
     },
     {
         id: Fszt2,
         max:3,
-        elso:Bori,
-        masodik: Dani,
-        harmadik: János,
-        
+        lakok:['Bori','Dani','János'] 
     },
+
     {
         id: Fszt3,
         max:3,
-        elso:Kati,
-        masodik: Szergely,
-        harmadik: Bogi,
-        negyedik: Janka
-        
+        lakok:['Kati','Szergely','Bogi','Janka']  
     },
+
     {
         id: Fszt4,
         max:3,
-        elso:Balázs,
-        masodik: Eszter,
-        harmadik: Boldizsár,
-        negyedik: Éva,
-        otodik: Luca
-        
+        lakok:['Balázs','Eszter','Boldizsár','Éva','Luca']        
     },
+
     {
         id: Fszt5,
         max:2,
-        elso:Ambrus,
+        lakok:['Ambrus']
     
     },
+
     {
         id: Fszt6,
         max:2,
-        elso:Marci,
-    
-        
+        lakok:['Marci']        
     },
+
     {
         id: Fszt7,
         max:2,
-        elso:Levi
-        
+        lakok:['Levi']       
     },
+
     {
         id: Fszt8,
         max:2,
-        elso:Julcsi,
-        masodik: Balu,
-        
+        lakok:['Julcsi','Balu'] 
     },
+
     {
         id: Em1,
         max:2,
-        elso:Ági,
+        lakok:['Ági']
         
     },
     {
         id: Em2,
         max:2,
-        elso:Panna,
-        masodik: Feri,
+        lakok:['Panna','Feri']
+        
         
     },
     {
         id: Em3,
         max:2,
-        elso:Bea,
-        masodik: Gergő,
+        lakok:['Bea','Gergő']
+        
         
     },
     {
         id: Em4,
         max:2,
-        elso:Tamás,
-        masodik: Jutka,
+        lakok:['Tamás','Jutka']
+        
     },
     {
         id: Em5,
         max:5,
-        elso:Péter,
-        masodik: Ilona,
-        harmadik: Lackó,
-        negyedik: Anna
+        lakok:['Péter','Ilona','Lackó','Anna']
+        
         
         
     },
     {
         id: Em6,
         max:5,
-        elso:Klára,
-        masodik: Ágoston,
-        harmadik: Botond,
-        negyedik: Csongor,
-        otodik: Zsombor
+        lakok:['Klára','Ágoston','Botond','Csongor','Zsombor']
+        
         
     },
     {
         id: Em7,
         max:4,
-        elso:János,
-        masodik: Titi,
-        harmadik: Bori,
-        negyedik: Balázs,
+        lakok:['János','Titi','Bori','Balázs']
+        
         
     },
     {
         id: Em8,
         max:3,
-        elso:Dorka,
-        masodik: Timi,
-        harmadik: Ilus
+        lakok:['Dorka','Timi','Ilus']
+        
         
     }
     
