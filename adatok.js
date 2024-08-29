5ca0a155864868c0a953e6dff524ebd5865a6be5
DDorka02 <dobszaydorka@gmail.com>
2024-08-29 14:13:01 +0200

    first commit

---
+++ adatok.js
@@ -0,0 +1,197 @@
+export const SZOBAK = [
+    {
+        id: F1/1,
+        max:2,
+        elso:Eszter,
+        masodik: Buszti,
+        harmadik: Sajka
+        
+    },
+
+    {
+        id: F1/2,
+        max:2,
+        elso:Dávid
+        
+    },
+
+    {
+        id: F2/1,
+        max:2,
+        elso:Luca,
+        masodik: Adorján
+        
+    },
+    {
+        id: F2/2,
+        max:2,
+        elso:Veronika,
+        masodik: Rozi,
+        harmadik: Adél
+        
+    },
+    {
+        id: F3/1,
+        max:2,
+        elso:Marcsi,
+        masodik: F. Matyi,
+        harmadik: Kinga 
+        
+    },
+    {
+        id: F3/2,
+        max:2,
+        elso:Bálint,
+        masodik: Noémi
+        
+    },
+    {
+        id: F3/1,
+        max:1,
+        elso:Matyi
+        
+    },
+    {
+        id: F4/1,
+        max:2,
+        elso:Miklós,
+        masodik: Timi,
+        harmadik: Beni
+        
+    },
+    {
+        id: F4/2,
+        max:2,
+        elso:Lívia,
+        masodik: Vili
+        
+    },
+    {
+        id: Fszt1,
+        max:3,
+        elso:Zsófi,
+        masodik: András,
+        harmadik: Marci,
+        negyedik: Misi,
+        otodik: Juli
+        
+    },
+    {
+        id: Fszt2,
+        max:3,
+        elso:Bori,
+        masodik: Dani,
+        harmadik: János,
+        
+    },
+    {
+        id: Fszt3,
+        max:3,
+        elso:Kati,
+        masodik: Szergely,
+        harmadik: Bogi,
+        negyedik: Janka
+        
+    },
+    {
+        id: Fszt4,
+        max:3,
+        elso:Balázs,
+        masodik: Eszter,
+        harmadik: Boldizsár,
+        negyedik: Éva,
+        otodik: Luca
+        
+    },
+    {
+        id: Fszt5,
+        max:2,
+        elso:Ambrus,
+    
+    },
+    {
+        id: Fszt6,
+        max:2,
+        elso:Marci,
+    
+        
+    },
+    {
+        id: Fszt7,
+        max:2,
+        elso:Levi
+        
+    },
+    {
+        id: Fszt8,
+        max:2,
+        elso:Julcsi,
+        masodik: Balu,
+        
+    },
+    {
+        id: Em1,
+        max:2,
+        elso:Ági,
+        
+    },
+    {
+        id: Em2,
+        max:2,
+        elso:Panna,
+        masodik: Feri,
+        
+    },
+    {
+        id: Em3,
+        max:2,
+        elso:Bea,
+        masodik: Gergő,
+        
+    },
+    {
+        id: Em4,
+        max:2,
+        elso:Tamás,
+        masodik: Jutka,
+    },
+    {
+        id: Em5,
+        max:5,
+        elso:Péter,
+        masodik: Ilona,
+        harmadik: Lackó,
+        negyedik: Anna
+        
+        
+    },
+    {
+        id: Em6,
+        max:5,
+        elso:Klára,
+        masodik: Ágoston,
+        harmadik: Botond,
+        negyedik: Csongor,
+        otodik: Zsombor
+        
+    },
+    {
+        id: Em7,
+        max:4,
+        elso:János,
+        masodik: Titi,
+        harmadik: Bori,
+        negyedik: Balázs,
+        
+    },
+    {
+        id: Em8,
+        max:3,
+        elso:Dorka,
+        masodik: Timi,
+        harmadik: Ilus
+        
+    }
+    
+
+]
